f8fb13200ee93ae341fd15b30c43f19fb3a908ed
weixin_45890414 <weixin_45890414@noreply.gitcode.com>
2026-03-16 14:45:25 +0800

    列插槽

--- src/package/table/src/TableColumn.vue
+++ src/package/table/src/TableColumn.vue
@@ -18,17 +18,16 @@
   >
     <!-- 表头插槽 -->
     <template v-slot:header>
-      <el-tooltip
-        v-if="column.prop"
-        :content="`${column.label} ${column.prop} ${column.show}`"
-        placement="top"
-      >
+      <el-tooltip v-if="column.prop" :content="`${column.label} ${column.prop}`" placement="top">
         <span>{{ column.label }}</span>
       </el-tooltip>
       <!-- 多级表头无prop无需提示 -->
       <span v-else>{{ column.label }}</span>
     </template>
 
+    <template v-for="(index, name) in $scopedSlots" v-slot:[name]="data">
+      <slot :name="name" v-bind="data"></slot>
+    </template>
     <!-- 递归渲染子列 -->
     <template v-if="column.children && column.children.length">
       <TableColumn v-for="(child, idx) in column.children" :key="idx" :column="child">
@@ -38,43 +37,41 @@
         </template>
       </TableColumn>
     </template>
-    <!-- 内容插槽：自定义渲染 -->
-    <template v-else-if="column.render" v-slot="scope">
-      <render-dom :render="() => column.render(scope.row)" />
-    </template>
-    <template v-else-if="column.renderSlot" v-slot="scope">
-      <slot :row="scope.row" :name="column.prop" />
+    <template v-slot="scope" v-if="!column.children">
+      <render-dom v-if="column.render" :render="() => column.render(scope.row)" />
+      <slot v-else-if="column.renderSlot" :row="scope.row" :name="column.prop" />
+      <span v-else>{{ scope.row[column.prop] }}</span>
     </template>
   </el-table-column>
 </template>
 <script>
-  export default {
-    name: "TableColumn",
-    components: {
-      renderDom: {
-        functional: true,
-        props: { render: Function },
-        render(h, ctx) {
-          return <div>{ctx.props.render()}</div>;
-        },
-      },
-      TableColumn: null, // 递归自身，见下方
-    },
-    props: {
-      column: { type: Object, required: true },
-    },
-    data() {
-      return {};
-    },
-    // beforeCreate() {
-    //   // 递归注册自身
-    //   this.$options.components.TableColumn = require("./TableColumn.vue").default;
-    // },
-    computed: {
-      shouldShow() {
-        if (this.column.hasOwnProperty("show")) return this.column.show;
-        return true;
+export default {
+  name: "TableColumn",
+  components: {
+    renderDom: {
+      functional: true,
+      props: { render: Function },
+      render(h, ctx) {
+        return <div>{ctx.props.render()}</div>;
       },
     },
-  };
+    TableColumn: null, // 递归自身，见下方
+  },
+  props: {
+    column: { type: Object, required: true },
+  },
+  data() {
+    return {};
+  },
+  // beforeCreate() {
+  //   // 递归注册自身
+  //   this.$options.components.TableColumn = require("./TableColumn.vue").default;
+  // },
+  computed: {
+    shouldShow() {
+      if (this.column.hasOwnProperty("show")) return this.column.show;
+      return true;
+    },
+  },
+};
 </script>
